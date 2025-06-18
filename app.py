234eb658377c1431bf0e807afb2565914414b073
Ayu-web-cpu <ayushsingh33695@gmail.com>
2025-06-18 16:39:41 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request
+import joblib
+
+app = Flask(__name__)
+
+# Load model and vectorizer
+model = joblib.load("model.pkl")
+vectorizer = joblib.load("vectorizer.pkl")
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    message = request.form['message']
+    data = [message]
+    vect = vectorizer.transform(data)
+    prediction_raw = model.predict(vect)[0]
+    
+    # Convert to label
+    prediction_label = "Spam" if prediction_raw == 1 else "Ham"
+
+    return render_template('index.html', prediction=prediction_label, original=message)
+
+if __name__ == '__main__':
+    app.run(debug=True)
